3ff83c8fcb538a8b9a0a2687c087e908fa9825bf
Sizzler <info@computerbits.dk>
2011-08-29 14:22:58 +0200

    Added a few files

---
+++ XAF/XAF/DeleteOrder.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XAF
+{
+    public partial class DeleteOrder : System.Web.UI.Page
+    {
+        int oID;
+        LogicManager logicMgr;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            logicMgr = LogicManager.GetLogicMgrInstance();
+        }
+
+        protected void btnDeleteOrder_Click(object sender, EventArgs e)
+        {
+            oID = Convert.ToInt32(txtOrderID.Text);
+
+            logicMgr.DeleteOrder(oID);
+        }
+    }
+}
